c6b362140a5c540525854de146deec61d01a4d65
vkhatri <vkhatri>
2014-07-30 01:40:18 +0530

    added auto maxmemory set

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,9 @@
 default[:activemq] = {
-  :cluster_name => 'default',
+  :cluster_name => 'default', # reserved attribute for future use with Chef Node Discovery
   :user         => 'activemq',
   :group        => 'activemq',
   :user_home    => nil,
-  :setup_user   => false,
+  :setup_user   => false, # manage user account
   :version      => '5.10.0',
   :install_dir  => '/usr/local/activemq',
   :data_dir     => '/opt/activemq',
@@ -13,6 +13,8 @@ default[:activemq] = {
   :pid_dir      => '/var/run/activemq', # solr service user pid dir
   :log_dir      => '/var/log/activemq',
   :templates_cookbook => "activemq", # template source cookbook
+  :auto_maxmemory     => true, # automatically calculate maximum memory 
+  :system_memory      => 768, # minimum reserved memory for the system, required when set :auto_maxmemory => true
 
   :limits => {
     :memlock    => 'unlimited',
@@ -26,8 +28,7 @@ default[:activemq] = {
   },
 
   :wrapper      => {
-    :init_memory  => 512,
-    :maxmemory    => 1024, # This needs to be RAM dependent
+    :init_memory  => 512
   },
 
   :stomp        => {
@@ -50,12 +51,13 @@ default[:activemq] = {
     :enable     => true,
     :port       => 61614
   },
+
   :config       => {
-    :enable_management  => 'true',
+    :enable_management      => 'true',
     :maxconns               => 10000,
     :maxframesize           => 104857600,
     :listen_address         => '0.0.0.0',
-    :pending_message_limit  => '1000'
+    :pending_message_limit  => '10000'
   }
 }
 
@@ -63,4 +65,26 @@ default[:activemq][:source_dir]      = "/usr/local/apache-activemq-#{node.active
 default[:activemq][:tarball][:url]   = "https://repository.apache.org/content/repositories/releases/org/apache/activemq/apache-activemq/#{node.activemq.version}/apache-activemq-#{node.activemq.version}-bin.tar.gz"
 default[:activemq][:tarball][:md5]   = 'bb83016ae899e0c8a346ffeee15a0390'
 
+if not node.activemq.auto_maxmemory
+  default[:activemq][:wrapper][:maxmemory]   = 1024
+else
+  # Calculate Java Xmx memory value
+  if node.memory and node.memory.has_key?('total')
+    total_memory    = node.memory.total.gsub('kB','').to_i / 1024
+    mem_per         = (total_memory.to_i % 1024)
+    system_memory   = if total_memory.to_i < 2048
+                        total_memory.to_i / 2
+                      else
+                        if mem_per >= node.activemq.system_memory.to_i
+                          mem_per
+                        else
+                          mem_per + 1024
+                        end
+                      end
+    java_memory     = total_memory.to_i - system_memory
+    # Covers total_memory / 2 condition, Always Keeping Java -Xmx even integer
+    java_memory     += 1 if not java_memory.even?
+    default[:activemq][:wrapper][:maxmemory] = java_memory
+  end
+end
 
